6015fb969a3aec1a8a0378b0b9cb7a92963aa0a0
Desocseronit <nesynovsavva7@gmail.com>
2026-04-13 16:34:19 +0300

    add documentation (ru)

--- CollectionElement.php
+++ CollectionElement.php
@@ -1,5 +1,12 @@
 <?php namespace core;
 
+/**
+ * Экземпляр класса CollectionElement представляет атомарное значение находящиеся в экземпляре класса Collection
+ * @setValue => устанавливает значение у экемпляра класса
+ * @getvalue => получает значение
+ * @stringify => приводит значение экземпляра класса к строчному типу данных
+ */
+
 class CollectionElement{
     private $_value = null;
 
