82886d9d02cc07c2c7dd54165c7ee0d996bf0d2f
Desocseronit <nesynovsavva7@gmail.com>
2026-04-20 23:24:24 +0300

    ja ebenat

--- Application.php
+++ Application.php
@@ -9,11 +9,7 @@ require_once('Record.php');
 require_once('User.php');
 require_once('Image.php');
 
-echo '<pre>';
-Database::instance('host=26.152.118.24 port=5432 dbname=404devs password=1 user=postgres');
-$a = new Request();
-$b = new Image($a->getinfo()->files->getValue()->image->getValue());
-print_r($b->getSize());
+
 
 class Application{
 
