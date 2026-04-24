b6cf3cbfcaa6373b05d6bfb4aea5cd7ea30507f7
Desocseronit <nesynovsavva7@gmail.com>
2026-04-24 17:09:33 +0300

    try to fix FoxickL code

--- core/actions/Votes.php
+++ core/actions/Votes.php
@@ -1,26 +1,39 @@
-<?php namespace core\actions
-    use core\{Post};
+<?php namespace core\actions;
+
+use core\{Response};
+use core\{Post};
+use core\{Answer};
+use core\{User};
+use core\{Database};
+
 class Votes{
     public function execute ($req){
         $body = $req->getInfo()->body->getValue();
         $user = User::find($body->userId);
+        $res;
         if(isset($body->postId)){
             $postId = $body->post_id;
-            Database::instance()->updateRecord(
+            if(Database::instance()->updateRecord(
                 'post',
                 ['votes'=>'votes + '.$body->value],
                 'id = $1',
                 [$postId]
-            );
+            )){
+                $res = new Response(200);
+            }
+            else{$res = new Response(500);}
         }
         elseif(isset($body->answerId)){
             $answerId = $body->answerId;
-            Database::instance()->updateRecord(
+            if(Database::instance()->updateRecord(
                 'answers',
                 ['votes'=>'votes + '.$body->value],
                 'id = $1',
                 [$answerId]
-            );
+            )){
+                $res = new Response(200);
+            }
+            else{$res = new Response(500);}
         }
     }
 }
