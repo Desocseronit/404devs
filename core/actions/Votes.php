565e9109863a08a67f7aa6de159a0a29a8887d77
Ярослав Шадт <sadta34@gmail.com>
2026-04-24 16:26:08 +0300

    add Votes action

---
+++ core/actions/Votes.php
@@ -0,0 +1,26 @@
+<?php namespace core\actions
+    use core\{Post};
+class NewVote{
+    public function execute ($req){
+        $body = $req->getInfo()->body->getValue();
+        $user = User::find($body->userId);
+        if(isset($body->postId)){
+            $postId = $body->post_id;
+            Database::instance()->updateRecord(
+                'post',
+                ['votes'=>'votes + '.$body->value],
+                'id = $1',
+                [$postId]
+            );
+        }
+        elseif(isset($body->answerId)){
+            $answerId = $body->answerId;
+            Database::instance()->updateRecord(
+                'answers',
+                ['votes'=>'votes + '.$body->value],
+                'id = $1',
+                [$answerId]
+            );
+        }
+    }
+}
