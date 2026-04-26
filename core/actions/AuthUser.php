f2eaff4283f68c21b34758f9a4499f56bf72eaff
FoxickL <sadta34@gmail.com>
2026-04-26 13:05:37 +0300

    modify

--- core/actions/AuthUser.php
+++ core/actions/AuthUser.php
@@ -8,13 +8,18 @@ class AuthUser{
         $body = $req->getInfo()->body->getValue();
 
         $res;
-        $userByPassword = User::authByCredentials($body->$username,$body->$password);
+        $userByPassword = User::authByCredentials($body->username,$body->password);
         $user = User::authByCookies();
         if($user){
             $res = new Response(200 , ['user' => $user]);
         }
         elseif($userByPassword){
             $res = new Response(200 , ['user' => $userByPassword]);
-        }     
+        } 
+        else {
+            $res = new Response(401 , ['error' => 'User not authenticated']);
+        }
+        
+        return $res;    
     }
 }
