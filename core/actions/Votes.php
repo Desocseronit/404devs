8fdffdee688519b0dcdf309c2839469d850353a5
Desocseronit <nesynovsavva7@gmail.com>
2026-04-27 23:25:47 +0300

    new votes action

--- core/actions/Votes.php
+++ core/actions/Votes.php
@@ -6,17 +6,30 @@ use core\{Response,Post,Answer,User,Database};
 class Votes{
     public function execute ($req){ 
         $body = $req->getInfo()->body->getValue();
-        // $user = User::find($body->userId->getValue()); придумать как сохранять лайки юзеров (1 таблица (id user_id ?post_id ?answer_id) vs 2 таблицы (1) id , user_id, post_id   2) id, user_id , answer_id))
+        $user = User::find($body->userId->getValue());
         $res = null;
         if(isset($body->postId)){
             if(Database::instance()->incrementField('posts', 'votes' , (int)$body->value->getValue(), 'id = $1', [$body->postId->getValue()])){
-                $res = new Response(200);
+                if(Database::instance()->selectRecord('voted_posts', '*' , [['user_id', '=' , $user->id],['post_id','=',$body->postId->getValue()]])->items()){
+                    $res = new Response(400, ["message" => 'already voted']);
+                }
+                else{
+                    Database::instance()->insertRecord('voted_posts', ['user_id' => $user->id , 'post_id' => $body->postId->getValue()]);
+                    $res = new Response(200);
+                }
+                
             }
             else{$res = new Response(500);}
         }
         elseif(isset($body->answerId)){
             if(Database::instance()->incrementField('answers', 'votes' , (int)$body->value->getValue(), 'id = $1', [$body->answerId->getValue()])){
-                $res = new Response(200);
+                if(Database::instance()->selectRecord('voted_answers', '*' , [['user_id', '=' , $user->id],['answer_id','=',$body->answerId->getValue()]])->items()){
+                    $res = new Response(400, ["message" => 'already voted']);
+                }
+                else{
+                    Database::instance()->insertRecord('voted_answers', ['user_id' => $user->id , 'answer_id' => $body->answerId->getValue()]);
+                    $res = new Response(200);
+                }
             }
             else{$res = new Response(500);}
         }
