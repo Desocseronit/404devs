4fd59e5592838c41042b581802088b8808a8c935
Desocseronit <nesynovsavva7@gmail.com>
2026-05-07 20:52:47 +0300

    just rename

--- core/controllers/UserController.php
+++ core/controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController{
         $user = null;
         if(!Application::$user || Application::$user->id != $userId) $user = User::find($userId);
         else $user = Application::$user;
-        \view\View::renderView(["userData" => $user->stringify()] , '/profileView.php');
+        \view\View::renderView(["userData" => $user->stringify()] , '/profile.php');
         $response = new Response(200);
         $response -> send();
     }
