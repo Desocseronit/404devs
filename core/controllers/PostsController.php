18225fc5db34420fac5510cbe07d7fee70063482
Desocseronit <nesynovsavva7@gmail.com>
2026-05-10 18:51:42 +0300

    add ability to create posts

--- core/controllers/PostsController.php
+++ core/controllers/PostsController.php
@@ -3,7 +3,9 @@ require_once(__DIR__.'/../View.php');
 require_once(__DIR__.'/../Application.php');
 require_once(__DIR__.'/../modules/Database.php');
 require_once(__DIR__.'/../modules/Post.php');
-use core\{Application , Database , Post};
+require_once(__DIR__.'/../actions/NewPost.php');
+use core\{Application , Response , Database , Post};
+use core\actions\{NewPost};
 class PostsController{
     public function allPosts($params){
         $page = $params['page'];
@@ -16,14 +18,25 @@ class PostsController{
         \view\View::renderView(['data' => $data] , '/allPosts.php');
     }
 
-    public function postRender($params){
-        $post = Post::find((int)$params['id']);
-        $page = $params['page'];
-        $filterBy = $params['filterby'];
-        $side = (bool)$params['side'];
-        $idSide = (bool)$params['idSide'];
-        $data = Answer::paginate($page , self::$perPage , $filterBy, $side , $idSide , $category , $level);
-        $data['parentPost'] = $post->getInfo();
-        \view\View::renderView(['test' => $data] , '/test.php');
+    public function createPostsRender(){
+        if(Application::$user) \view\View::renderView(path: '/createPost.php');
+        else {
+            require_once('ErrorController.php');
+            $errorConInstance = new ErrorController();
+            $errorConInstance->render401();
+        }
+    }
+
+    public function createPost(){
+        $actionInstance = new NewPost();
+        $res = $actionInstance->execute();
+        $resp;
+        if(!$res){
+            $resp = new Response(400);
+        }
+        else{
+            $resp = new Response(200 , ['postId' => $res->id]);
+        }
+        $resp->send();
     }
 }
--- core/routeConfig.php
+++ core/routeConfig.php
@@ -4,13 +4,24 @@ require_once 'modules/Post.php';
 use core\{Application , Post};
 return [
     //[type , uri , controller , viewName , dependencies [paramName => [defaultValue , [allowed values?] , [not allowed values?]] , auth = null (if need auth)]
+    
+    //Posts
     ['type'=>'GET' , 'uri'=>'main' , 'controller'=>'PostsController' , 'view'=>'allPosts' , 'dependencies' => ['page' => [1 , [] , []] , 'filterby' => ['votes' , ['votes' , 'created_at' , 'views'] , []] , 'category' => [null , ['PHP' , 'JAVA' , 'Go' , 'Pyhton' , 'Node.js' , 'HTML' , 'CSS', 'JavaScript'] , []], 'level' => [null , ['studying' , 'easy' , 'medium' , 'hard' , 'esoteric'] , []], 'side' => [0 , [0 , 1] , []] , 'idSide' => [0 , [0 , 1] , []]]],
     ['type'=>'GET' , 'uri'=>'all-posts' , 'controller'=>'PostsController' , 'view'=>'allPosts' , 'dependencies' => ['page' => [1 , [] , []] , 'filterby' => ['votes' , ['votes' , 'created_at' , 'views'] , []] , 'category' => [null , ['PHP' , 'JAVA' , 'Go' , 'Pyhton' , 'Node.js' , 'HTML' , 'CSS', 'JavaScript'] , []], 'level' => [null , ['studying' , 'easy' , 'medium' , 'hard' , 'esoteric'] , []], 'side' => [0 , [0 , 1] , []] , 'idSide' => [0 , [0 , 1] , []]]],
+    ['type' => 'GET' , 'uri'=>'create-post' , 'controller' => 'PostsController' , 'view' => 'createPostsRender' , 'dependencies' => []],
+    ['type' => 'POST' , 'uri'=>'create-post' , 'controller' => 'PostsController' , 'view' => 'createPost' , 'dependencies' => []],
+
+    //User
+    ['type' => 'POST' , 'uri'=>'sign-up' , 'controller' => 'UserController' , 'view' => 'regUser' , 'dependencies' => []],
     ['type' => 'GET' , 'uri'=>'login' , 'controller' => 'UserController' , 'view' => 'loginRender' , 'dependencies' => []],
     ['type' => 'POST' , 'uri'=>'login' , 'controller' => 'UserController' , 'view' => 'authUser' , 'dependencies' => []],
     ['type' => 'GET' , 'uri'=>'sign-up' , 'controller' => 'UserController' , 'view' => 'regRender' , 'dependencies' => []],
     ['type' => 'POST' , 'uri'=>'sign-up' , 'controller' => 'UserController' , 'view' => 'regUser' , 'dependencies' => []],
     ['type' => 'GET' , 'uri'=>'profile' , 'controller' => 'UserController' , 'view' => 'profileRender' , 'dependencies' => ['id'  => [Application::$user->id ?? null, [], []]]],
+
+    // Answers
     ['type' => 'GET' , 'uri'=>'view-post' , 'controller' => 'AnswersController' , 'view' => 'postRender' , 'dependencies' => ['id' => [Post::getRandomPostId() , [] , []],'page' => [1 , [] , []] , 'filterby' => ['votes' , ['votes' , 'created_at'] , []], 'side' => [0 , [0 , 1] , []] , 'idSide' => [0 , [0 , 1] , []]]],
+
+    //errors
     'errors' => [['error' => 404 , 'view' => 'render404']]
 ];
