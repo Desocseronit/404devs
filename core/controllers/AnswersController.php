4e855feefd792c5245b0d61bb88cd905c0aff4e2
Desocseronit <nesynovsavva7@gmail.com>
2026-05-11 14:34:49 +0300

    add ability to add answers to posts

--- core/controllers/AnswersController.php
+++ core/controllers/AnswersController.php
@@ -4,7 +4,9 @@ require_once(__DIR__.'/../Application.php');
 require_once(__DIR__.'/../modules/Database.php');
 require_once(__DIR__.'/../modules/Post.php');
 require_once(__DIR__.'/../modules/Answer.php');
-use core\{Application , Database , Post , Answer};
+require_once(__DIR__.'/../actions/NewAnswer.php');
+use core\{Application , Response , Database , Post , Answer};
+use core\actions\{NewAnswer};
 class AnswersController{
     public function postRender($params){
         $post = Post::find((int)$params['id']);
@@ -15,5 +17,25 @@ class AnswersController{
         $data = Answer::paginate($page , Application::$CONFIG['publicationsPerPage'] , $filterBy, $side , $idSide , (int)$params['id']);
         $data['parentPost'] = $post->getInfo();
         \view\View::renderView(['test' => $data] , '/test.php');
+    }
+
+    public function addAnswer(){
+        if(!Application::$user){
+            require_once('ErrorController.php');
+            $errorConInstance = new ErrorController();
+            $errorConInstance->render401();
+            return;
+        }
+
+        $actionInstance = new NewAnswer();
+        $res = $actionInstance->execute();
+        $resp;
+        if($res){
+            $resp = new Response(201);
+        }
+        else{
+            $resp = new Response(400);
+        }
+        $resp->send();
     }
 }
--- core/routeConfig.php
+++ core/routeConfig.php
@@ -12,7 +12,6 @@ return [
     ['type' => 'POST' , 'uri'=>'create-post' , 'controller' => 'PostsController' , 'view' => 'createPost' , 'dependencies' => []],
 
     //User
-    ['type' => 'POST' , 'uri'=>'sign-up' , 'controller' => 'UserController' , 'view' => 'regUser' , 'dependencies' => []],
     ['type' => 'GET' , 'uri'=>'login' , 'controller' => 'UserController' , 'view' => 'loginRender' , 'dependencies' => []],
     ['type' => 'POST' , 'uri'=>'login' , 'controller' => 'UserController' , 'view' => 'authUser' , 'dependencies' => []],
     ['type' => 'GET' , 'uri'=>'sign-up' , 'controller' => 'UserController' , 'view' => 'regRender' , 'dependencies' => []],
@@ -21,6 +20,7 @@ return [
 
     // Answers
     ['type' => 'GET' , 'uri'=>'view-post' , 'controller' => 'AnswersController' , 'view' => 'postRender' , 'dependencies' => ['id' => [Post::getRandomPostId() , [] , []],'page' => [1 , [] , []] , 'filterby' => ['votes' , ['votes' , 'created_at'] , []], 'side' => [0 , [0 , 1] , []] , 'idSide' => [0 , [0 , 1] , []]]],
+    ['type' => 'POST' , 'uri'=>'add-answer' , 'controller' => 'AnswersController' , 'view' => 'addAnswer' , 'dependencies' => []],
 
     //errors
     'errors' => [['error' => 404 , 'view' => 'render404']]
